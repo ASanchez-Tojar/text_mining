8a28f0518b9867498f7338a6914e6e968ae601f0
ASanchez-Tojar <asanchez@orn.mpg.de>
2017-02-12 10:42:33 +0100

    generating word cloud

--- text_mining.R
+++ text_mining.R
@@ -35,3 +35,54 @@ library("RColorBrewer")
 # Clear memory and get to know where you are
 rm(list=ls())
 #getwd()
+
+#importing text file
+text <- readLines(file.choose())
+
+#loading the data as a corpus
+docs <- Corpus(VectorSource(text))
+
+#inspect content
+inspect(docs)
+
+
+#transforming text: replacing special characters
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+
+#cleaning text
+
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# # Remove your own stop word
+# # specify your stopwords as a character vector
+# docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+
+#building a term-document matrix
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+
+
+#generate the word cloud
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
